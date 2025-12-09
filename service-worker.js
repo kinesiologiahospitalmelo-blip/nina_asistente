c5bb6e76470899e7b0aa44a2775f3666c58cc9c9
kinesiologiahospitalmelo-blip <kinesiologia.hospital.melo@gmail.com>
2025-12-09 08:15:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,13 @@
-const CACHE = "nina-cache-v3";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./css/nina.css",
-  "./js/nina.js",
-
-  "./js/modulos/voz.js",
-  "./js/modulos/wakeword.js",
-  "./js/modulos/identidadUsuario.js",
-  "./js/modulos/comandosOffline.js",
-  "./js/modulos/comandosOnline.js",
-  "./js/modulos/aprendizaje.js",
-  "./js/modulos/buscador.js",
-  "./js/modulos/gps.js",
-  "./js/modulos/llamadas.js",
-  "./js/modulos/ia.js"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+self.addEventListener("install", (e) => {
+  console.log("SW instalado");
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
-    )
-  );
+self.addEventListener("activate", (e) => {
+  console.log("SW activado");
+  clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+self.addEventListener("fetch", (event) => {
+  // No hacemos cache, solo permitimos que la PWA sea válida
 });
